3450b4712f55a088c9b41358d0dfab7c6a3af8c1
syn <vicdemilly@gmail.com>
2019-04-11 10:05:52 +0200

    Autocorrection et ajout de commentaires

---
+++ main.js
@@ -0,0 +1,28 @@
+function getBlur(){
+
+alert('Merci de votre participation')
+}
+/*function getBlur() {
+  let message = 'Merci de votre participation';
+  document.getElementById("lastname").blur(message);
+  //alert('Merci de votre participation')*/
+
+
+/*function getFocus() {
+  document.getElementById("lastname").focus();
+}*/
+
+/*function sortie(){
+let unfocus = 'Merci pour votre participation';
+alert(unfocus);
+}*/
+
+/* const form = document.getElementById('lastname');
+
+form.addEventListener('focusin', (event) => {
+  event.target.style.background = 'pink';
+});
+
+form.addEventListener('focusout', (event) => {
+  event.target.style.background = '';
+}); */
