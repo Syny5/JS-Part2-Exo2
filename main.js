5ab97cfd4461357419feb5f20349958b751a54f7
syn <vicdemilly@gmail.com>
2019-04-11 10:08:13 +0200

    Ajout de commentaires

--- main.js
+++ main.js
@@ -2,27 +2,6 @@ function getBlur(){
 
 alert('Merci de votre participation')
 }
-/*function getBlur() {
-  let message = 'Merci de votre participation';
-  document.getElementById("lastname").blur(message);
-  //alert('Merci de votre participation')*/
 
-
-/*function getFocus() {
-  document.getElementById("lastname").focus();
-}*/
-
-/*function sortie(){
-let unfocus = 'Merci pour votre participation';
-alert(unfocus);
-}*/
-
-/* const form = document.getElementById('lastname');
-
-form.addEventListener('focusin', (event) => {
-  event.target.style.background = 'pink';
-});
-
-form.addEventListener('focusout', (event) => {
-  event.target.style.background = '';
-}); */
+// La fonction nommée getBlur dans le onblur permet ici d'afficher une boîte de dialogue (alert) lorsque l'on perd le focus
+// cliquez sur le champs puis cliquer en dehors pour perdre le focus
