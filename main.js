297bb02620f8dc3c91a8155d6da647d8bedff544
syn <vicdemilly@gmail.com>
2019-04-15 14:07:27 +0200

    Correction syntaxe

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function getBlur(){
-
 alert('Merci de votre participation')
 }
 
 // La fonction nommée getBlur dans le onblur permet ici d'afficher une boîte de dialogue (alert) lorsque l'on perd le focus
-// cliquez sur le champs puis cliquer en dehors pour perdre le focus
+// cliquez sur le champs puis cliquez en dehors pour perdre le focus
